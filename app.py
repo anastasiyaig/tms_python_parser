52f949de6c5368aff0704e1d4b2a9f4195bf3499
Anastasiya S <nastya@status.im>
2022-08-27 21:35:15 +0300

    added more methods to display
    1. added more methods to enhance disk data and cpu data collection
    2. reformatted the code

--- app.py
+++ app.py
@@ -2,21 +2,36 @@ import psutil
 import fontstyle
 
 
-def cpu_frequency():
+def cpu_data():
     res = {}
-    data = psutil.cpu_freq()
-    res.update(cpu_current=data.current, cpu_min=data.min, cpu_max=data.max)
+    # CPU frequency as a named tuple including current, min and max frequencies in Mhz
+    data_cpu_freq = psutil.cpu_freq()
+    res.update(cpu_current=data_cpu_freq.current, cpu_min=data_cpu_freq.min, cpu_max=data_cpu_freq.max)
+    # The number of physical cores multiplied by the number of threads that can run on each core
+    res.update(cpu_count=psutil.cpu_count())
     return res
 
 
-def disk_usage():
+def disks_data():
     res = {}
-    data = psutil.disk_usage('/')
+    # Return disk usage statistics about the partition which contains the given path as a named tuple including total,
+    # used and free space expressed in bytes, plus the percentage usage.
+    data_disk_usage = psutil.disk_usage('/')
     res.update(
-        disk_total_space=(round(data.total / (1024 ** 3), 2)),
-        disk_space_used=(round(data.used / (1024 ** 3), 2)),
-        disk_space_free=(round(data.free / (1024 ** 3), 2)),
-        disk_used_percentage=data.percent)
+        disk_total_space=(round(data_disk_usage.total / (1024 ** 3), 2)),
+        disk_space_used=(round(data_disk_usage.used / (1024 ** 3), 2)),
+        disk_space_free=(round(data_disk_usage.free / (1024 ** 3), 2)),
+        disk_used_percentage=data_disk_usage.percent)
+    # Return system-wide disk I/O statistics as a named tuple
+    data_io_counters = psutil.disk_io_counters(perdisk=False, nowrap=True)
+    res.update(
+        disk_read_count=data_io_counters.read_count,
+        disk_write_count=data_io_counters.write_count,
+        disk_read_bytes=(round(data_io_counters.read_bytes / (1024 ** 3), 2)),  # to gigabytes
+        disk_write_bytes=(round(data_io_counters.write_bytes / (1024 ** 3), 2)),  # to gigabytes
+        disk_read_time=(round(data_io_counters.read_time / 60000)),  # from milliseconds to minutes
+        disk_write_time=(round(data_io_counters.write_time / 60000)),  # from milliseconds to minutes
+    )
     return res
 
 
@@ -30,29 +45,41 @@ def sensors_data():
     return res
 
 
-def display_data():
-    cpu_freq = cpu_frequency()
-    print(fontstyle.apply('Your CPU frequency data:', 'bold/blue'))
-    print("CPU current | CPU Minimum | CPU Maximum")
-    print("________________________________________")
-    print("   {:^8} |   {:^8}  |    {:^8}\n".format(cpu_freq['cpu_current'], cpu_freq['cpu_min'], cpu_freq['cpu_max']))
+def display_data(cpu=None, disks=None, sensors=None):
+    print(fontstyle.apply("Your CPU aggregated data is present below:", "bold/blue"))
+    print("CPU current | CPU Minimum | CPU Maximum | CPU Count")
+    print("___________________________________________________")
+    print("{0:^8}Mhz |{1:^8} Mhz |{2:^8}Mhz  |{3:^8}\n".format
+          (cpu['cpu_current'],
+           cpu['cpu_min'],
+           cpu['cpu_max'],
+           cpu['cpu_count']))
 
-    disk_usage_data = disk_usage()
-    print(fontstyle.apply('Your disk usage data:', 'bold/DARKCYAN'))
-    print("Total space | Used space | Free space | Used space %")
-    print("____________________________________________________")
-    print("{:<} GB   | {:<}  GB  | {:<}  GB | {:<} %\n".format(
-        disk_usage_data['disk_total_space'],
-        disk_usage_data['disk_space_used'],
-        disk_usage_data['disk_space_free'],
-        disk_usage_data['disk_used_percentage']
+    disk_info_header = str("Total space | Used space | Free space | Used space, % | Read count | Write count |"
+                           "Bytes read, GB | Bytes written, GB | Read time, min | Write time, min")
+    print("_" * (len(disk_info_header)))
+    print(fontstyle.apply('Your disk aggregated data:', 'bold/DARKCYAN'))
+    print(disk_info_header)
+
+    print(" {0:<8}GB | {1:<8}GB | {2:<8}GB | {3:^12}% | {4:^11}| {5:^12}| {6:^14}| {7:^18}| {8:^15}| {9:^15}"
+        .format(
+        disks['disk_total_space'],
+        disks['disk_space_used'],
+        disks['disk_space_free'],
+        disks['disk_used_percentage'],
+        disks['disk_read_count'],
+        disks['disk_write_count'],
+        disks['disk_read_bytes'],
+        disks['disk_write_bytes'],
+        disks['disk_read_time'],
+        disks['disk_write_time']
     ))
+    print("_" * (len(disk_info_header)))
 
-    sensors = sensors_data()
     print(fontstyle.apply('Your battery information:', 'bold/RED'))
-    print("Power left | Time remaining | Charging status")
-    print("______________________________________________")
-    print("{:<} %       | {:<}  minute(s) | {:<} \n".format(
+    print("Power left, % | Time remaining | Charging status")
+    print("_"*48)
+    print("{:^14}|{:^5} minute(s) | {:<} \n".format(
         sensors['battery_power_left'],
         sensors['seconds_left_to_discharge'],
         sensors['cable_connected']
@@ -60,7 +87,11 @@ def display_data():
 
 
 def main():
-    display_data()
+    display_data(
+        cpu_data(),
+        disks_data(),
+        sensors_data()
+    )
 
 
 if __name__ == "__main__":
