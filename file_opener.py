7995ea8bd4b48dc16b8953f82ef65a556499c016
Anastasiya S <nastya@status.im>
2022-09-07 18:02:18 +0300

    added a script which opens a file in csv and reads it

---
+++ file_opener.py
@@ -0,0 +1,25 @@
+import json
+
+
+def my_parser():
+    with open('parseMe.csv', 'r') as f:
+
+        text_lines = []  # empty list to store lines from the csv file
+        for line in f.readlines():
+            text_lines.append(line.rstrip().split(";"))  # filling up the list with lines, separated by ;
+
+        keys = list(text_lines[0])  # getting 1st line as keys for dictionary
+
+        values = []  # empty list to create values that will be mapped to keys
+        for i in range(1, len(text_lines)):  # starting from 1 to skip the header line
+            values.append(dict(zip(keys, text_lines[i])))
+        values = json.dumps(values)  # convert the list with dictionaries to JSON format
+        return values
+
+
+def main():
+    print(my_parser())
+
+
+if __name__ == "__main__":
+    main()
