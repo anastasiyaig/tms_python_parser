c5741b27a3b4afd7f443e1633e57230c0c36010d
Anastasiya S <nastya@status.im>
2022-09-08 18:16:31 +0300

    optimised code, get rid of unnecessary cycle

--- file_opener.py
+++ file_opener.py
@@ -2,20 +2,17 @@ import json
 
 
 def my_parser():
+    values = []  # empty list to create values that will be mapped to keys
     with open('parseMe.csv', 'r') as f:
-
-        text_lines = []  # empty list to store lines from the csv file
-        for line in f.readlines():
-            text_lines.append(line.rstrip().split(";"))  # filling up the list with lines, separated by ;
-
-        keys = list(text_lines[0])  # getting 1st line as keys for dictionary
-
-        values = []  # empty list to create values that will be mapped to keys
-        for i in range(1, len(text_lines)):  # starting from 1 to skip the header line
-            values.append(dict(zip(keys, text_lines[i])))
-        with open("sample.json", "w") as outfile:
-            json.dump(values, outfile, sort_keys=True, indent=4)
-        return values
+        for index, line in enumerate(f.readlines()):
+            text_line = line.rstrip().split(";")  #
+            if index == 0:
+                keys = text_line
+            else:
+                values.append(dict(zip(keys, text_line)))
+    with open("sample.json", "w") as outfile:
+        json.dump(values, outfile, indent=4)
+    return values
 
 
 def main():
